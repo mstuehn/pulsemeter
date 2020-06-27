e3a64fc1ebaf61de6836a424f655455b2c0c1557
Manuel Stuehn <freebsd@justmail.de>
2020-06-27 12:25:56 +0000

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,8 @@ usage(void)
     exit(1);
 }
 
-static std::atomic<uint32_t> liters;
+static float water_m3 = 0.0;
+static float gas_m3 = 0.0;
 
 int main( int argc, char* argv[] )
 {
@@ -76,7 +77,7 @@ int main( int argc, char* argv[] )
     MQTT mqtt(root["mqtt-configuration"]);
     auto base_topic = root["base_topic"].asString();
 
-    mqtt.add_callback( base_topic+"/cmd/m3", [](uint8_t*msg, size_t len)
+    mqtt.add_callback( base_topic+"/water/cmd/m3", [](uint8_t*msg, size_t len)
             {
                 Json::Value msgval;
                 Json::Reader rd;
@@ -87,7 +88,26 @@ int main( int argc, char* argv[] )
 
                 if( parsingOk ){
                     try {
-                        liters.exchange( msgval["set"].asInt() );
+                        water_m3 = msgval["set"].asFloat();
+                    }catch( Json::Exception e ) {
+                        printf("catched Exception: %s\n", e.what() );
+                    }
+                }
+
+            } );
+
+    mqtt.add_callback( base_topic+"/gas/cmd/m3", [](uint8_t*msg, size_t len)
+            {
+                Json::Value msgval;
+                Json::Reader rd;
+                std::string strmsg( (char*)msg, len );
+                printf("Got message: %s\n", strmsg.c_str() );
+                std::istringstream in(strmsg);
+                bool parsingOk = rd.parse(in , msgval, false);
+
+                if( parsingOk ){
+                    try {
+                        gas_m3 = msgval["set"].asFloat();
                     }catch( Json::Exception e ) {
                         printf("catched Exception: %s\n", e.what() );
                     }
@@ -123,17 +143,29 @@ int main( int argc, char* argv[] )
                 uint16_t code, value;
                 if( get_events( fd, EV_KEY, &code, &value ) && value == 1 )
                 {
+                    Json::StreamWriterBuilder wr;
+                    wr.settings_["precision"] = 3;
+
                     switch( code ) {
+                        case KEY_F21:
+                        {
+                            gas_m3 += 0.01;
+
+                            Json::Value info;
+                            info["m3"] = gas_m3;
+                            std::string msg = Json::writeString(wr, info);
+                            mqtt.publish(base_topic+"/gas/values", msg.c_str(), msg.length(), 0 );
+                            std::cout << "Water: " << water_m3 << std::endl;
+                        }break;
                         case KEY_F22:
                         {
-                            liters += 100;
+                            water_m3 += 0.1;
 
-                            Json::StreamWriterBuilder wr;
-                            wr.settings_["precision"] = 3;
                             Json::Value info;
-                            info["m3"] = (float)liters/1000.0;
+                            info["m3"] = water_m3;
                             std::string msg = Json::writeString(wr, info);
-                            mqtt.publish(base_topic+"/values", msg.c_str(), msg.length(), 0 );
+                            mqtt.publish(base_topic+"/water/values", msg.c_str(), msg.length(), 0 );
+                            std::cout << "Gas: " << water_m3 << std::endl;
                         } break;
                     }
             }
